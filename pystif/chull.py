d1961921397ac3a37b016a660a6d1d085cb45dbd
Thomas Gläßle <t_glaessle@gmx.de>
2015-06-17 16:33:55 +0200

    Implement incremental CHM algorithm in python
    NOTE: not yet making use of the ConvexHull incremental mode as there
    seems to be some error causing a crash… :(

--- pystif/chull.py
+++ pystif/chull.py
@@ -25,6 +25,7 @@ from docopt import docopt
 from .core.lp import Problem
 from .core.it import elemental_inequalities, num_vars
 from .util import format_vector, scale_to_int, VectorMemory, print_to
+from .xray import find_xray
 
 
 def principal_components(data_points, s_limit=1e-10):
@@ -46,66 +47,76 @@ def add_left_zero(mat):
     return np.hstack((np.zeros((mat.shape[0], 1)), mat))
 
 
-def convex_hull(xrays):
+def convex_hull_method(lp, lpb, rays,
+                       report_ray, report_yes,
+                       status_info, qinfo):
 
-    points = del_const_col(xrays)
+    points = del_const_col(rays)
     points = np.vstack((np.zeros(points.shape[1]), points))
 
     # Now make sure the dataset lives in a full dimensional subspace
-    principal_basis, subordinate_basis = principal_components(points)
-    points = np.dot(points, principal_basis)
+    subspace, nullspace = principal_components(points)
 
-    hull = scipy.spatial.ConvexHull(points)
-    faces = hull.equations
-
-    faces = np.array([
-        eq[:-1] for eq in faces
-        if abs(eq[-1]) < 1e-5])
-
-    # add back the removed dimensions
-    faces = np.dot(faces, principal_basis.T)
-
-    nullspace = subordinate_basis.T
+    nullspace = nullspace.T
     nullspace = add_left_zero(nullspace)
-    faces = add_left_zero(faces)
-    return np.vstack((
-        nullspace,
-        -nullspace,
-        # The following is an empirical minus sign. I didn't find anything on
-        # the qhull documentation as to how the equations are oriented, but
-        # this seems to work. As soon as things start failing, you might want
-        # to take a second look.
-        -faces,
-    ))
+    for face in nullspace:
+        report_yes(face)
+        report_yes(-face)
 
+    if nullspace.shape[0] == 0:
+        subspace = np.eye(points.shape[1])
 
-def unique_rows(L):
-    """Return new matrix with the unique rows of L."""
-    seen = VectorMemory()
-    L = list(map(scale_to_int, L))
-    return np.array([q for q in L if not seen(q)])
+    # initial hull
+    points = np.dot(points, subspace)
+    qinfo(len(points))
+    hull = scipy.spatial.ConvexHull(points, incremental=True)
 
-
-def classify(lp, ineqs, report_yes, report_no, status_info):
-    """
-    Classify inequalities according to whether they are implied by the LP.
-
-    All inequalities will be fed to either ``report_yes`` or ``report_no``
-    depending on whether they are implied.
-
-    Returns ``True`` if all inequalities are valid.
-    """
-    total = len(ineqs)
     yes = 0
-    for i, q in enumerate(ineqs):
-        status_info(i, total, yes)
-        if lp.implies(q, embed=True):
-            yes += 1
-            report_yes(q)
+    seen = VectorMemory()
+    seen_ray = VectorMemory()
+    for ray in rays:
+        seen_ray(ray)
+
+    while True:
+        new_points = []
+        faces = hull.equations
+        total = faces.shape[0]
+        for i, face in enumerate(faces):
+            if abs(face[-1]) > 1e-5:
+                continue
+            status_info(i, total, yes)
+
+            # The following is an empirical minus sign. I didn't find anything
+            # on the qhull documentation as to how the equations are oriented,
+            # but apparently points x inside the convex hull are described by
+            # ``face ∙ (x,1) ≤ 0``
+            face = -face[:-1]
+            face = np.dot(face, subspace.T)
+            face = np.hstack((0, face))
+            face = scale_to_int(face)
+
+            if seen(face):
+                continue
+
+            if lp.implies(face, embed=True):
+                yes += 1
+                report_yes(face)
+            else:
+                ray = find_xray(lpb, face[1:])
+                if seen_ray(ray):
+                    continue
+                report_ray(ray)
+                point = np.dot(ray[1:], subspace)
+                new_points.append(point)
+
+        if new_points:
+            points = np.vstack((points, new_points))
+            qinfo(len(points))
+            hull.add_points(new_points, restart=True)
         else:
-            report_no(q)
+            break
+
     status_info(total, total, yes)
-    return yes == total
 
 
 def print_vector(print_, q):
@@ -116,10 +127,14 @@ def print_vector(print_, q):
 def print_status(print_, i, total, yes):
     """Print status."""
     l = len(str(total))
-    print_("Progress: {}/{},  valid: {}"
-           .format(str(i).rjust(l), total, str(yes).rjust(l)))
+    print_("Progress: {}/{} ({} facets)"
+           .format(str(i).rjust(l), total, yes))
     if i == total:
         print_("\n")
+
+
+def print_qhull(print_, num_points):
+    print_("qhull on {} rays\n".format(num_points))
 
 
 def main(args=None):
@@ -129,17 +144,27 @@ def main(args=None):
     xrays = np.loadtxt(opts['--xrays'], ndmin=2)
     lp = Problem(system)
 
-    faces = convex_hull(xrays)
-    faces = unique_rows(faces)
+    lpb = Problem(system)
+
+    subdim = xrays.shape[1]
+    limit = 1
+    for i in range(1, subdim):
+        lpb.set_col_bnds(i, 0, limit)
+
+
 
     feedback = print_to(opts['--feedback'], '#', default=sys.stdout)
     output = print_to(opts['--output'])
+
     info = partial(print, '\r', end='', file=sys.stderr)
 
-    callbacks = (partial(print_vector, output),
-                 partial(print_vector, feedback),
-                 partial(print_status, info))
-    return classify(lp, faces, *callbacks)
+    callbacks = (partial(print_vector, feedback),
+                 partial(print_vector, output),
+                 partial(print_status, info),
+                 partial(print_qhull, info))
+
+    convex_hull_method(lp, lpb, xrays, *callbacks)
+    return True
 
 
 if __name__ == '__main__':
