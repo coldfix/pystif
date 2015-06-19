ce734ca332f673e8450d2fcefab51b3dc319a281
Thomas Gläßle <t_glaessle@gmx.de>
2015-06-19 17:41:35 +0200

    Preliminary grouping mechanism for pretty
    This does not quite guarantee that constraints in the same group are
    permutations of each other, but it comes close — and at least it looks
    nicer!

--- pystif/pretty.py
+++ pystif/pretty.py
@@ -2,15 +2,17 @@
 Print equations in human readable format.
 
 Usage:
-    pretty -i INPUT [-o OUTPUT] [-c]
+    pretty -i INPUT [-o OUTPUT] [-c] [-g]
 
 Options:
     -i INPUT, --input INPUT         Input file
     -o OUTPUT, --output OUTPUT      Output file
     -c, --canonical                 Assume canonical column labels
+    -g, --group                     Group similar constraints
 """
 
 
+from collections import Counter, defaultdict
 from math import log2
 from docopt import docopt
 from .core.it import num_vars
@@ -50,6 +52,37 @@ def format_human_readable(constraint, columns):
     return "{} ≥ {}".format(" ".join(lhs), _fmt_float(rhs))
 
 
+def vars_from_colname(column_name):
+    # TODO: more sophisticated parsing?
+    return frozenset(column_name)
+
+
+def character(vals, varsets, allvars):
+    """
+    Return a value that characterizes the constraint 'vals' in the sense that
+    constraints that are permutations of each other are guaranteed to have the
+    same character.
+
+    However, it is not guaranteed that constraints with the same character are
+    indeed permutations of each other.
+    """
+    return frozenset(Counter([
+        frozenset((coef, len(varsets[i]))
+                  for i, coef in enumerate(vals)
+                  if coef != 0 and var in varsets[i])
+        for var in allvars
+    ]).items())
+
+
+def group_permuted_terms(constraints, colnames):
+    varsets = [frozenset()] + [vars_from_colname(n) for n in colnames]
+    allvars = frozenset.union(*varsets)
+    by_character = defaultdict(list)
+    for c in constraints:
+        by_character[character(c, varsets, allvars)].append(c)
+    return by_character.values()
+
+
 def main(args=None):
     opts = docopt(__doc__, args)
     system = System.load(opts['--input'])
@@ -60,8 +93,16 @@ def main(args=None):
     else:
         columns = default_column_labels(system.dim)
     print_ = print_to(opts['--output'])
-    for row in system.matrix:
-        print_(format_human_readable(row, columns))
+    def dump(rows):
+        for row in rows:
+            print_(format_human_readable(row, columns))
+    if opts['--group']:
+        first = True
+        for g in group_permuted_terms(system.matrix, columns):
+            first = not first and print_()
+            dump(g)
+    else:
+        dump(system.matrix)
 
 
 if __name__ == '__main__':
