522918093ad64055fde68033a0b254797f05ef77
Thomas Gläßle <t_glaessle@gmx.de>
2015-08-30 15:25:52 +0200

    Minor modification to example file

--- example/bell2x2.sh
+++ example/bell2x2.sh
@@ -12,12 +12,17 @@ data=$here/data
 # create system of elemental inequalities:
 makesys -e -v "A a B b" -o init.txt
 
+subs="AB Ab aB ab A a B b"
+symm="Aa <> aA; AaBb <> BbAa"
+
 # use different elimination methods:
-time chm init.txt -s "AB Ab aB ab A a B b" -o fin-chm.txt
-time fme init.txt -s "AB Ab aB ab A a B b" -o fin-fme.txt
-time afi init.txt -s "AB Ab aB ab A a B b" -o fin-afi.txt -q
-time afi init.txt -s "AB Ab aB ab A a B b" -o fin-sym.txt -q \
-    -y "Aa <> aA; AaBb <> BbAa"
+time chm init.txt -s "$subs" -o fin-chm.txt
+time fme init.txt -s "$subs" -o fin-fme.txt
+time afi init.txt -s "$subs" -o fin-afi.txt -q
+time afi init.txt -s "$subs" -o fin-sym.txt -q  -y "$symm"
+
+time rfd init.txt -s "$subs" -o fin-rfd.txt -y "$symm"
+time rfd init.txt -s "$subs" -o fin-rfd.txt -y "$symm" -d 7
 
 # consistency check
 equiv init.txt $data/init-bell2x2.txt
