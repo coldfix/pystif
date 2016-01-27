d4fb2ecb188d8533a7c64dcfc85f3e2f36d0ea36
Thomas Gläßle <t_glaessle@gmx.de>
2016-01-27 03:15:34 +0100

    Add second quiet level for 'equiv'

--- pystif/equiv.py
+++ pystif/equiv.py
@@ -2,7 +2,7 @@
 Check the equivalence of two system of equations.
 
 Usage:
-    equiv [-q] [-1] A B
+    equiv [-q...] [-1] A B
 
 Options:
     -q, --quiet         No output, result is only signaled via return value
@@ -26,25 +26,21 @@ from docopt import docopt
 from .core.io import format_vector, System
 
 
-def show_missing_constraints(lp, constraints, name_a, name_b):
-    """Show the difference of these constraints."""
-    missing = [constr for constr in constraints.matrix
-               if not lp.implies(constr)]
-    if not missing:
-        print("{} implies {}!".format(name_a, name_b))
-        return 0
-    print("{} does not imply the following inequalities of {}:"
-          .format(name_a, name_b))
-    for constr in missing:
-        print(format_vector(constr))
-    return len(missing)
-
-
 def check_implies(sys_a, sys_b, name_a, name_b, quiet=False):
     lp = sys_a.lp()
-    if quiet:
-        return lp.implies(sys_b.matrix)
-    return show_missing_constraints(lp, sys_b, name_a, name_b) == 0
+    missing = [constr for constr in sys_b.matrix
+               if not lp.implies(constr)]
+    if quiet <= 1:
+        if missing:
+            print("{} does not imply {}!".format(name_a, name_b))
+        else:
+            print("{} implies {}!".format(name_a, name_b))
+    if quiet == 0:
+        print("{} misses the following inequalities of {}:"
+                .format(name_a, name_b))
+        for constr in missing:
+            print(format_vector(constr))
+    return len(missing) == 0
 
 
 def main(args=None):
