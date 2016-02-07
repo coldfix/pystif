4c4ea6b0d695fb72ed2a76bcb9a604cf406607c1
Thomas Gläßle <t_glaessle@gmx.de>
2016-02-07 20:57:38 +0100

    Remove approximate grouping from 'pretty'
    Symmetries are now easy enough to use since they can be defined in the
    input files.

--- pystif/pretty.py
+++ pystif/pretty.py
@@ -2,22 +2,17 @@
 Print equations in human readable format.
 
 Usage:
-    pretty INPUT [-o OUTPUT] [-g | -y SYM] [-q]
+    pretty INPUT [-o OUTPUT] [-y SYM] [-q]
 
 Options:
     -o OUTPUT, --output OUTPUT      Output file
-    -g, --group                     Group similar constraints
     -y SYM, --symmetry SYM          Specify symmetry group generators
     -q, --quiet                     Less output in the presence of symmetries
 """
 
-
-from collections import Counter, defaultdict
-
 from docopt import docopt
 
-from .core.io import (System, print_to, VectorMemory,
-                      default_column_labels, column_varname_labels)
+from .core.io import System, print_to, default_column_labels
 from .core.symmetry import SymmetryGroup, group_by_symmetry
 
 
@@ -52,37 +47,6 @@ def format_human_readable(constraint, columns):
     return "{} ≥ 0".format(" ".join(lhs).lstrip('+ '))
 
 
-def vars_from_colname(column_name):
-    # TODO: more sophisticated parsing?
-    return frozenset(column_name)
-
-
-def character(vals, varsets, allvars):
-    """
-    Return a value that characterizes the constraint 'vals' in the sense that
-    constraints that are permutations of each other are guaranteed to have the
-    same character.
-
-    However, it is not guaranteed that constraints with the same character are
-    indeed permutations of each other.
-    """
-    return frozenset(Counter([
-        frozenset((coef, len(varsets[i]))
-                  for i, coef in enumerate(vals)
-                  if coef != 0 and var in varsets[i])
-        for var in allvars
-    ]).items())
-
-
-def group_permuted_terms(constraints, colnames):
-    varsets = [frozenset()] + [vars_from_colname(n) for n in colnames]
-    allvars = frozenset.union(*varsets)
-    by_character = defaultdict(list)
-    for c in constraints:
-        by_character[character(c, varsets, allvars)].append(c)
-    return by_character.values()
-
-
 def main(args=None):
     opts = docopt(__doc__, args)
     system = System.load(opts['INPUT'])
@@ -98,12 +62,7 @@ def main(args=None):
     symmetries = opts['--symmetry']
     if symmetries is None:
         symmetries = system.symmetries
-    if opts['--group']:
-        first = True
-        for g in group_permuted_terms(system.matrix, columns):
-            first = not first and print_()
-            dump(g)
-    elif symmetries:
+    if symmetries:
         sg = SymmetryGroup.load(symmetries, system.columns)
         groups = group_by_symmetry(sg, system.matrix)
         for g in groups:
