a4907544e03b095e066d6ccad6b665179c68493b
Thomas Gläßle <t_glaessle@gmx.de>
2016-03-21 02:03:36 +0100

    Add utility scripts to start qviol calculations

---
+++ tools/start_qviol_second.zsh
@@ -0,0 +1,34 @@
+
+prefix=example/data/qviol-bell3x2
+
+# 08D
+ts qviol ${prefix}-08D-222-none.yml -d 222 -o ${prefix}-08D-222-chshe.yml -c CHSHE
+ts qviol ${prefix}-08D-222-none.yml -d 222 -o ${prefix}-08D-222-chsh.yml -c CHSH
+ts qviol ${prefix}-08D-222-none.yml -d 222 -o ${prefix}-08D-222-ppt.yml -c SEP
+
+ts qviol ${prefix}-08D-333-none.yml -d 333 -o ${prefix}-08D-333-chshe.yml -c CHSHE
+ts qviol ${prefix}-08D-333-none.yml -d 333 -o ${prefix}-08D-333-cglmp.yml -c CGLMP
+
+# 12D
+ts qviol ${prefix}-12D-222-none.yml -d 222 -o ${prefix}-12D-222-chshe.yml -c CHSHE
+ts qviol ${prefix}-12D-222-none.yml -d 222 -o ${prefix}-12D-222-chsh.yml -c CHSH
+ts qviol ${prefix}-12D-222-none.yml -d 222 -o ${prefix}-12D-222-ppt.yml -c SEP
+
+ts qviol ${prefix}-12D-333-none.yml -d 333 -o ${prefix}-12D-333-chshe.yml -c CHSHE
+ts qviol ${prefix}-12D-333-none.yml -d 333 -o ${prefix}-12D-333-cglmp.yml -c CGLMP
+
+# 18D
+ts qviol ${prefix}-18D-222-none.yml -d 222 -o ${prefix}-18D-222-chshe.yml -c CHSHE
+ts qviol ${prefix}-18D-222-none.yml -d 222 -o ${prefix}-18D-222-chsh.yml -c CHSH
+ts qviol ${prefix}-18D-222-none.yml -d 222 -o ${prefix}-18D-222-ppt.yml -c SEP
+
+ts qviol ${prefix}-18D-333-none.yml -d 333 -o ${prefix}-18D-333-chshe.yml -c CHSHE
+ts qviol ${prefix}-18D-333-none.yml -d 333 -o ${prefix}-18D-333-cglmp.yml -c CGLMP
+
+# 26D
+ts qviol ${prefix}-26D-222-none.yml -d 222 -o ${prefix}-26D-222-chshe.yml -c CHSHE
+ts qviol ${prefix}-26D-222-none.yml -d 222 -o ${prefix}-26D-222-chsh.yml -c CHSH
+ts qviol ${prefix}-26D-222-none.yml -d 222 -o ${prefix}-26D-222-ppt.yml -c SEP
+
+# ts qviol ${prefix}-26D-333-none.yml -d 333 -o ${prefix}-26D-333-chshe.yml -c CHSHE
+# ts qviol ${prefix}-26D-333-none.yml -d 333 -o ${prefix}-26D-333-cglmp.yml -c CGLMP
