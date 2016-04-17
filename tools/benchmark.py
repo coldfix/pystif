71e29785ab4a061e7e701daf0d2a09bedf479336
Thomas Gläßle <t_glaessle@gmx.de>
2016-04-17 22:16:38 +0200

    Improve benchmark output format

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -3,7 +3,7 @@ Compare runtimes of AFI/CHM by projecting the given input systems to random
 subspaces of various dimensions.
 
 Usage:
-    benchmark INPUT... [-o OUTPUT] [-s DIMS] [-n NUM] [-l LOGS]
+    benchmark INPUT [-o OUTPUT] [-s DIMS] [-n NUM] [-l LOGS]
 
 Options:
     -o OUTPUT, --output OUTPUT      Output file
@@ -26,36 +26,42 @@ import yaml
 from pystif.core.io import System
 
 COLUMNS = (
-    'in_dim',
-    'in_rows',
-    'out_dim',
-    'afi_time',
-    'chm_time',
-    'num_facets',
-    'num_ridges',
-    'num_vertices',
-    'ridges_per_facet',
-    'vertices_per_facet',
-    'vertices_per_ridge',
-    'afi_symm_time',
-    'chm_symm_time',
-    'afi_symm_num_facets',
-    'afi_symm_num_ridges',
-    'afi_symm_num_vertices',
+    (' d',          'out_dim'),
+    ('   t_afi',    'afi_time'),
+    ('   t_chm',    'chm_time'),
+    ('   f',        'num_facets'),
+    ('   r',        'num_ridges'),
+    ('   v',        'num_vertices'),
+    ('     r_f',    'ridges_per_facet'),
+    ('     v_f',    'vertices_per_facet'),
+    ('     v_r',    'vertices_per_ridge'),
+    ('   _tafi',    'afi_symm_time'),
+    ('   _tchm',    'chm_symm_time'),
+    ('  _f',        'fafi_symm_num_facets'),
+    ('  _r',        'afi_symm_num_ridges'),
+    ('  _v',        'afi_symm_num_vertices'),
 )
 
 
 def main(argv=None):
     opts = docopt(__doc__, argv)
-    input_files = opts['INPUT']
+    input_file = opts['INPUT']
     output_dims = sorted(set(parse_range(opts['--subspaces'])))
     num_runs = int(opts['--num-runs'])
     logdir = opts['--logs']
     os.makedirs(logdir, exist_ok=True)
-    tasks = make_tasks(input_files, output_dims, num_runs, logdir+'/')
+    system = System.load(input_file)
+    tasks = make_tasks(system, input_file, output_dims, num_runs, logdir+'/')
     output_file = _open_for_writing(opts['--output'])
     print_ = partial(_print, file=output_file, flush=True)
-    print("#", *COLUMNS, file=output_file, flush=True)
+    print_('# input system:')
+    print_('#  - file:', input_file)
+    print_('#  - rows:', system.shape[0])
+    print_('#  - dim: ', system.shape[1])
+    cols_short, cols_long = zip(*COLUMNS)
+    print_('#', *cols_long)
+    print_()
+    print_('#', *cols_short)
     for task in tasks:
         exec_task(task, print_)
 
@@ -75,43 +81,38 @@ def get_random_subspace(dim, subdim):
     return random.sample(range(dim), subdim)
 
 
-def make_tasks(input_files, output_dims, num_runs, prefix):
-    input_systems = [System.load(f) for f in input_files]
+def make_tasks(system, filename, output_dims, num_runs, prefix):
     return (
         Task(filename, system, get_random_subspace(system.dim, subdim), i, prefix)
-        for filename, system in zip(input_files, input_systems)
         for subdim in output_dims
         for i in range(num_runs))
 
 
 def exec_task(task, print_):
-    afi = single_pass(task, 'afi', '-y', '', '-r1')
-    chm = single_pass(task, 'chm', '-y', '')
-    afi_symm = single_pass(task, 'afi', '-r1')
-    chm_symm = single_pass(task, 'chm')
+    afi_pure = single_pass(task, 'afi', 'pure', '-y', '', '-r1')
+    chm_pure = single_pass(task, 'chm', 'pure', '-y', '')
+    afi_symm = single_pass(task, 'afi', 'symm', '-r1')
+    chm_symm = single_pass(task, 'chm', 'symm')
 
-    if not afi or not chm:
+    if not afi_pure or not chm_pure or not afi_symm or not chm_symm:
         print_(
-            dim,
-            num_rows,
             len(task.subspace),
-            bool(afi),
-            bool(chm),
+            bool(afi_pure),
+            bool(chm_pure),
+            bool(afi_symm),
+            bool(chm_symm),
         )
         return
-    num_rows, dim = task.system.shape
     print_(
-        dim,
-        num_rows,
         len(task.subspace),
-        afi['time'],
-        chm['time'],
-        afi['num_facets'],
-        afi['num_ridges'],
-        afi['num_vertices'],
-        afi['ridges_per_facet'][0],
-        afi['vertices_per_facet'][0],
-        afi['vertices_per_ridge'][0],
+        afi_pure['time'],
+        chm_pure['time'],
+        afi_pure['num_facets'],
+        afi_pure['num_ridges'],
+        afi_pure['num_vertices'],
+        afi_pure['ridges_per_facet'][0],
+        afi_pure['vertices_per_facet'][0],
+        afi_pure['vertices_per_ridge'][0],
         afi_symm['time'],
         chm_symm['time'],
         afi_symm['num_facets'],
@@ -120,9 +121,9 @@ def exec_task(task, print_):
     )
 
 
-def single_pass(task, method, *cmd_args):
+def single_pass(task, method, extra, *cmd_args):
     space = " ".join(map(str, task.subspace))
-    basename = '{}{}-{}'.format(task.prefix, task.name, method)
+    basename = '{}{}-{}-{}'.format(task.prefix, task.name, extra, method)
     outf = basename + '.txt'
     logf = basename + '.log'
     inff = basename + '.yml'
@@ -168,7 +169,11 @@ def _open_for_writing(filename):
 
 
 def _fmt(arg):
-    return '{:.3f}'.format(arg) if isinstance(arg, float) else arg
+    if isinstance(arg, float):
+        return '{:8.3f}'.format(arg)
+    if isinstance(arg, int):
+        return '{:4}'.format(arg)
+    return arg
 
 
 def _print(*args, **kwargs):
